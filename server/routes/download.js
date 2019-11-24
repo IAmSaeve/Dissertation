e1874507c7321f7b7fc192ec3dbe4ccfe0bd6d9c
Sebastian Petersen <sebkirller@gmail.com>
2019-11-24 22:45:59 +0100

    Fiddle with form data streams

---
+++ server/routes/download.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/:id', function (req, res, next) {
+    res.status(200).send(req.params.id);
+});
+
+module.exports = router;
