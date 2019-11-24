df6692b3acb375114be7df3ae9490db7818da8f9
michaelSteinmejer <33892912+michaelSteinmejer@users.noreply.github.com>
2019-11-24 16:27:36 +0100

    Imports
    NO REQUIRE

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import './App.scss';
-const crypto = require('crypto');
+import crypto from 'crypto';
 
 function App() {
   function c() {
