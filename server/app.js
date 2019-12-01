3160a228dcd37c47fbc7198045f0d2f061bb7a52
Sebastian Petersen <sebkirller@gmail.com>
2019-12-01 22:11:34 +0100

    Comments

--- server/app.js
+++ server/app.js
@@ -2,14 +2,17 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors')
 
+// Import routes
 const uploadRouter = require('./routes/upload');
 const downloadRouter = require('./routes/download');
 
 const app = express();
 
+// Middleware
 app.use(logger('dev'));
 app.use(cors({ origin: false }))
 
+// Register routes
 app.use('/upload', uploadRouter);
 app.use('/download', downloadRouter);
 
