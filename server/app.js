808199bedb88db157cd5e329a2ecba4cb80bd417
Sebastian Petersen <sebkirller@gmail.com>
2019-12-01 22:11:24 +0100

    Dependencies

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const path = require('path');
 const logger = require('morgan');
+const cors = require('cors')
 
 const uploadRouter = require('./routes/upload');
 const downloadRouter = require('./routes/download');
@@ -8,9 +8,7 @@ const downloadRouter = require('./routes/download');
 const app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({ origin: false }))
 
 app.use('/upload', uploadRouter);
 app.use('/download', downloadRouter);
