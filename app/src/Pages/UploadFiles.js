4cd2ea22d2b458c3a09ea0da08b8c9cd5be4905d
michaelSteinmejer <33892912+michaelSteinmejer@users.noreply.github.com>
2019-12-16 06:01:53 +0100

    Arrow func
    turned into arrow function

--- app/src/Pages/UploadFiles.js
+++ app/src/Pages/UploadFiles.js
@@ -7,7 +7,7 @@ class Uploadfiles extends Component {
   // creates a context
   static contextType = UploadContext;
 
-  bytesToSize(bytes) {
+  bytesToSize = (bytes) => {
     var sizes = ["Bytes", "KB", "MB", "GB", "TB"];
     if (bytes == 0) return "0 Byte";
     var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
