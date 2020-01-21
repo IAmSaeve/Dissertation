7047e9aeb2235e178f251920e17eac832fd0e0fc
Sebastian Petersen <sebkirller@gmail.com>
2020-01-21 20:46:12 +0100

    Fragment and new import

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
 import RouterOutput from "./Router/RouterOutput";
-import "./Styles/base.scss";
+import "./Styles/bundle.scss";
 import UploadContextProvider from "./Contexts/UploadContext";
 import Header from "./Router/Header";
 
 class App extends Component {
   render() {
     return (
-      <div className="mainGrid">
+      <>
         <UploadContextProvider>
           <Header className="header" />
           <RouterOutput className="body" />
         </UploadContextProvider>
-      </div>
+      </>
     );
   }
 }
