b689ae744bd6927c7a447d2fe4db4321311f71c1
Sebastian Petersen <sebkirller@gmail.com>
2020-01-23 18:29:41 +0100

    Update router

--- app/src/Router/RouterOutput.jsx
+++ app/src/Router/RouterOutput.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { Switch, Route, Redirect, BrowserRouter } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import UploadContainer from "../Pages/UploadContainer";
 import Download from "../Pages/Download";
+import About from "../Pages/About";
+import Project from "../Pages/Project";
 
 const Router = () => (
-  <>
-    <BrowserRouter>
-      <Switch>
-        <Route exact path='/Upload' component={UploadContainer} />
-        <Route exact path='/Download/:id' component={Download} />
-        <Redirect to="/Upload" />
-      </Switch>
-    </BrowserRouter>
-  </>
+  <Switch>
+    <Route exact path='/Upload' component={UploadContainer} />
+    <Route exact path='/About' component={About} />
+    <Route exact path='/Project' component={Project} />
+    <Route exact path='/Download/:id' component={Download} />
+    <Redirect to="/Upload" />
+  </Switch>
 );
 
 export default Router;
